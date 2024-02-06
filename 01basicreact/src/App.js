5aa2b47c62cecc58e1ab046450e50494c54d6046
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-02-06 17:55:41 +0530

    create your own react libraray

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./chai";
+function App() {
+  return (
+    <>
+      <Chai/>
+      <h1>Welcome in React Series | Ashad</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+function App() {
+
+  const username = "chai aur code"
+
+  return (
+    <>
+        <h1>React with Vite | Ashad | {username} </h1>
+        <Chai/>
+    </>
+  )
+}
+export default App
