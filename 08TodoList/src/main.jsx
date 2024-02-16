3eb0c72aecaf81249e6a75787c66b5d01553e8a9
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-02-16 11:51:58 +0530

    login

---
+++ 08TodoList/src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { 
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+ } from "react-router-dom";
+import LayOut from "./LayOut.jsx";
+import Home from "./Component/Home/Home.jsx";
+import TodoList from "./Component/TodoList/TodoList.jsx";
+import Contact from "./Component/Contact/Contact.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<LayOut />}>
+      <Route path="" element={<Home />} />
+      <Route path="todo" element={<TodoList />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
